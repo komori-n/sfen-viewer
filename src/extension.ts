84d9177f8c805a97666e924081de0f64eead152e
komori-n <ikamat.kmr@gmail.com>
2022-09-12 20:59:02 +0900

    feat: add svg based renderer (wip)

--- src/extension.ts
+++ src/extension.ts
@@ -2,18 +2,13 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-import { Color, Shogi } from 'shogi.js';
-import sharp = require('sharp');
+import { Shogi } from 'shogi.js';
+import { Buffer } from 'buffer';
 import { Kind } from 'shogi.js/dist/src/Kind';
-
-const CELL_SIZE=24;
-const HAND_HEIGHT=18;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export async function activate(context: vscode.ExtensionContext) {
-	const imageBuilder = new ImageBuilder;
-	await imageBuilder.init();
 	let disposable = vscode.languages.registerHoverProvider("*", {
 		async provideHover(document, position) {
 			const range = document.getWordRangeAtPosition(position, new RegExp("[a-zA-Z0-9 /+-]+"));
@@ -32,9 +27,9 @@ export async function activate(context: vscode.ExtensionContext) {
 			const shogi = new Shogi;
 			shogi.initializeFromSFENString(sfen.toString());
 
-			const b = await imageBuilder.build(shogi);
-
-			return new vscode.Hover(new vscode.MarkdownString(`![](data:image/png;base64,${b.toString("base64")})`));
+			let svg = render(shogi);
+			let svgString = Buffer.from(svg).toString('base64');
+			return new vscode.Hover(new vscode.MarkdownString(`![](data:image/svg+xml;base64,${svgString})`));
 		}
 	});
 
@@ -44,191 +39,145 @@ export async function activate(context: vscode.ExtensionContext) {
 // this method is called when your extension is deactivated
 export function deactivate() {}
 
-// A class that creates a image. It holds image buffers to reduce latency at runtime.
-class ImageBuilder {
-	private pieceMap: Map<Kind, Buffer> = new Map<Kind, Buffer>;
-	private pieceMapR: Map<Kind, Buffer> = new Map<Kind, Buffer>;
-	private black: Buffer | null = null;
-	private white: Buffer | null = null;
-	private turn: Buffer | null = null;
-	private background: Buffer | null = null;
-	private largeBackground: Buffer | null = null;
-	private initialized: boolean = false;
+const pieceMap = new Map<Kind, String>([
+	["FU", "歩"],
+	["KY", "香"],
+	["KE", "桂"],
+	["GI", "銀"],
+	["KI", "金"],
+	["KA", "角"],
+	["HI", "飛"],
+	["OU", "玉"],
+	["TO", "と"],
+	["NY", "杏"],
+	["NK", "杏"],
+	["NG", "全"],
+	["UM", "馬"],
+	["RY", "龍"],
+]);
 
-	async init() {
-		this.pieceMap.set("FU", makePieceImage("歩"));
-		this.pieceMap.set("KY", makePieceImage("香"));
-		this.pieceMap.set("KE", makePieceImage("桂"));
-		this.pieceMap.set("GI", makePieceImage("銀"));
-		this.pieceMap.set("KI", makePieceImage("金"));
-		this.pieceMap.set("KA", makePieceImage("角"));
-		this.pieceMap.set("HI", makePieceImage("飛"));
-		this.pieceMap.set("OU", makePieceImage("玉"));
-		this.pieceMap.set("TO", makePieceImage("と"));
-		this.pieceMap.set("NY", makePieceImage("杏"));
-		this.pieceMap.set("NK", makePieceImage("杏"));
-		this.pieceMap.set("NG", makePieceImage("全"));
-		this.pieceMap.set("UM", makePieceImage("馬"));
-		this.pieceMap.set("RY", makePieceImage("龍"));
-		for (const pt of this.pieceMap.keys()) {
-			const image = await sharp(this.pieceMap.get(pt)).rotate(180).toBuffer();
-			this.pieceMapR.set(pt, image);
-		}
+function render(shogi: Shogi): String {
+	console.log(shogi);
 
-		this.black = makePieceImage("☗");
-		this.white = makePieceImage("☖");
+	const CELL_SIZE=24;
+	const FONT_SIZE=18;
+	const HAND_HEIGHT=18;
+	const HAND_FONT_SIZE=14;
+	const HALF_CELL_SIZE = CELL_SIZE / 2;
+	const IMAGE_WIDTH = CELL_SIZE * 11 + 4;
+	const DEFAULT_IMAGE_HEIGHT = CELL_SIZE * 9 + 1;
+	const TEXT_COLOR = "white";
 
-		this.background = await this.buildBackground(11 * CELL_SIZE + 1, 9 * CELL_SIZE);
-		this.largeBackground = await this.buildBackground(11 * CELL_SIZE + 1, 12 * CELL_SIZE);
+	let elements: Array<String> = [];
+	let height = DEFAULT_IMAGE_HEIGHT;
 
-		this.turn = await sharp({
-			create: {
-				width: CELL_SIZE,
-				height: CELL_SIZE,
-				channels: 4,
-				background: "#ffff0066",
+	// board
+	for (let i = 0; i < 10; ++i) {
+		elements.push(lineElement(CELL_SIZE, i * CELL_SIZE, 10 * CELL_SIZE, i * CELL_SIZE, TEXT_COLOR));
+		elements.push(lineElement((i + 1) * CELL_SIZE, 0, (i + 1) * CELL_SIZE, 9 * CELL_SIZE, TEXT_COLOR));
+	}
+
+	// pieces on the board
+	for (let j = 0; j < 9; ++j) {
+		for (let i = 0; i < 9; ++i) {
+			let p = shogi.board[8 - i][j];
+			if (p === null) {
+				continue;
 			}
-		}).png().toBuffer();
 
-		this.initialized = true;
-	}
+			const color = p.color;
+			const pt = p.kind;
 
-	async build(shogi: Shogi) {
-		if (!this.initialized) {
-			await this.init();
+			elements.push(textElement(
+				pieceMap.get(pt) ?? "",
+				(i + 1) * CELL_SIZE + HALF_CELL_SIZE,
+				(j + 1) * CELL_SIZE - 5,
+				FONT_SIZE,
+				TEXT_COLOR,
+				!!color));
 		}
-
-		let {composites, isLarge} = this.buildHand(shogi);
-		composites = composites.concat(this.buildBoard(shogi));
-
-		const board = (isLarge ? this.largeBackground : this.background) as Buffer;
-		return sharp(board).composite(composites).png().toBuffer();
 	}
 
-	private buildBoard(shogi: Shogi) {
-		let composites = [];
-		for (let j = 0; j < 9; ++j) {
-			for (let i = 0; i < 9; ++i) {
-				const p = shogi.board[8 - i][j];
-				if (p === null) {
-					continue;
-				}
+	// hand pieces
+	elements.push(textElement("☖", HALF_CELL_SIZE - 3, HAND_HEIGHT, HAND_HEIGHT, TEXT_COLOR));
+	elements.push(textElement("☗", CELL_SIZE * 10 + HALF_CELL_SIZE + 3, HAND_HEIGHT, HAND_HEIGHT, TEXT_COLOR));
+	if (shogi.turn) {
+		elements.push(`<rect x="0" y="0" width="${HAND_HEIGHT}" height="${HAND_HEIGHT + 4}" fill="#ffff0066"/>`);
+	} else {
+		elements.push(`<rect x="${10 * CELL_SIZE + 6}" y="0" width="${HAND_HEIGHT}" height="${HAND_HEIGHT + 4}" fill="#ffff0066"/>`);
+	}
+	for (let i = 0; i < 2; ++i) {
+		const hand = shogi.hands[i];
 
-				const color = p.color;
-				const pt = p.kind;
-				const pieceImage = color ? this.pieceMapR.get(pt) : this.pieceMap.get(pt);
-				composites.push({
-					input: pieceImage,
-					left: (i + 1) * CELL_SIZE,
-					top: (j) * CELL_SIZE,
-				});
+		let h = 1;
+		for (let pt of pieceMap.keys()) {
+			const count = hand.filter(function(x) { return x.kind === pt; }).length;
+			if (count === 0) {
+				continue;
 			}
-		}
-		return composites;
-	}
 
-	private buildHand(shogi: Shogi) {
-		let composites = [];
-		composites.push({
-			input: this.black as Buffer,
-			left: 10 * CELL_SIZE,
-			top: 0
-		});
-		composites.push({
-			input: this.white as Buffer,
-			left: 0,
-			top: 0
-		});
-
-		let isLarge = false;
-		for (let i = 0; i < 2; ++i) {
-			const hand = shogi.hands[i];
-
-			let h = 1;
-			for (let pt of this.pieceMap.keys()) {
-				const count = hand.filter(function(x) { return x.kind === pt; }).length;
-				if (count === 0) {
-					continue;
-				}
-
-				composites.push({
-					input: this.pieceMap.get(pt),
-					left: i === 0 ? 10 * CELL_SIZE : 0,
-					top: h * HAND_HEIGHT
-				});
+			elements.push(textElement(
+				pieceMap.get(pt) ?? "",
+				(1 - i) * 10 * CELL_SIZE + HALF_CELL_SIZE - (i * 6 - 3),
+				(h + 1) * HAND_HEIGHT,
+				HAND_FONT_SIZE,
+				TEXT_COLOR));
+			h++;
+			if (count > 1) {
+				elements.push(textElement(
+					count.toString(),
+					(1 - i) * 10 * CELL_SIZE + HALF_CELL_SIZE - (i * 6 - 3),
+					(h + 1) * HAND_HEIGHT,
+					HAND_FONT_SIZE,
+					TEXT_COLOR));
 				h++;
-				if (count > 1) {
-					composites.push({
-						input: makePieceImage(count.toString()),
-						left: i === 0 ? 10 * CELL_SIZE : 0,
-						top: h * HAND_HEIGHT
-					});
-					h++;
-				}
-			}
-
-			if (h > 11) {
-				isLarge = true;
 			}
 		}
-
-		composites.push({
-			input: this.turn as Buffer,
-			left: shogi.turn === Color.Black ? 10 * CELL_SIZE : 0,
-			top: 0,
-		});
-		return {composites, isLarge};
-	}
-
-	private async buildBackground(width: Number, height: Number) {
-		const verticalLine = await sharp({
-			create: {
-				width: 1,
-				height: 9 * CELL_SIZE,
-				channels: 3,
-				background: "#000000",
-			}
-		}).png().toBuffer();
-		const horizontalLine = await sharp({
-			create: {
-				width: 9 * CELL_SIZE,
-				height: 1,
-				channels: 3,
-				background: "#000000",
-			}
-		}).png().toBuffer();
-
-		let composites = [];
-		for (let i = 0; i < 10; ++i) {
-			composites.push({
-				input: verticalLine,
-				left: (i + 1) * CELL_SIZE,
-				top: 0
-			});
-			composites.push({
-				input: horizontalLine,
-				left: CELL_SIZE,
-				top: i * CELL_SIZE,
-			});
+		if (height < (h + 1) * HAND_HEIGHT) {
+			height = (h + 1) * HAND_HEIGHT;
 		}
-
-		return await sharp({
-			create: {
-				width: width as number,
-				height: height as number,
-				channels: 4,
-				background: "#FFFFFFFF",
-			}
-		}).composite(composites).png().toBuffer();
 	}
+
+	let svg = `<svg
+		width="${IMAGE_WIDTH}"
+		height="${height}"
+		xmlns="http://www.w3.org/2000/svg">`;
+	for (let i = 0; i < elements.length; ++i) {
+		svg += elements[i];
+	}
+	svg += `</svg>`;
+
+	return svg;
 }
 
-function makePieceImage(s: String) {
-	return Buffer.from(`
-		<svg width="${CELL_SIZE}" height="${CELL_SIZE}">
-			<text x="50%" y="50%" text-anchor="middle" dy="0.43em" font-size="16" fill="#000" font-family="serif">${s}</text>
-		</svg>
-	`);
+function lineElement(x1: Number, y1: Number, x2: Number, y2: Number, color: String): String {
+	return `<line x1="${x1}" y1="${y1}" x2="${x2}" y2="${y2}" stroke="${color}"/>`;
+}
+
+function textElement(text: String, x: Number, y: Number, size: Number, color: String, rev: boolean = false): String {
+	if (rev) {
+		return `
+			<text
+				font-size="${size}"
+				fill="${color}"
+				text-anchor="middle"
+				font-family="serif"
+				transform="translate(${x},${(+y) - (+size) + 4}) rotate(180)"
+			>
+				${text}
+			</text>`;
+	} else {
+		return `
+			<text
+				x="${x}"
+				y="${y}"
+				font-size="${size}"
+				fill="${color}"
+				font-family="serif"
+				text-anchor="middle">
+				${text}
+			</text>`;
+	}
 }
 
 // Check if `str` contains SFEN string. If so, this function will return the found SFEN string.
